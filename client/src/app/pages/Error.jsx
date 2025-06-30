f5c4a60e208ff64fee246329bae9074468e5fcf4
jeremysr <jeremysr@protonmail.com>
2025-06-29 23:24:58 -0600

    prettier

--- client/src/app/pages/Error.jsx
+++ client/src/app/pages/Error.jsx
@@ -7,12 +7,23 @@ export default function ErrorPage() {
 
   return (
     <Container>
-      <Typography component={"h1"} variant="h2" align={"center"} sx={{ m: 4, mb:20, }}>
+      <Typography
+        component={"h1"}
+        variant="h2"
+        align={"center"}
+        sx={{ m: 4, mb: 20 }}
+      >
         something went wrong
       </Typography>
-      <Typography variant="h2" align="center">{error.status}</Typography>
-      <Typography variant="body1" align="center" sx={{mb:20,}}>{error?.statusText}</Typography>
-      <Typography variant="body2" align="center" sx={{mb:80,}}>{error.data}</Typography>
+      <Typography variant="h2" align="center">
+        {error.status}
+      </Typography>
+      <Typography variant="body1" align="center" sx={{ mb: 20 }}>
+        {error?.statusText}
+      </Typography>
+      <Typography variant="body2" align="center" sx={{ mb: 80 }}>
+        {error.data}
+      </Typography>
     </Container>
   );
 }
