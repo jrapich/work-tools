8a3aff89f502a54410fa31614f12e8e623fe1d31
jeremysr <jeremysr@protonmail.com>
2025-06-29 23:36:42 -0600

    simple footer

--- client/src/global/components/Footer.jsx
+++ client/src/global/components/Footer.jsx
@@ -2,29 +2,25 @@ import { Link, Box, Typography } from "@mui/material";
 
 function Copyright() {
   return (
-    <Typography variant="body2" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        powered by React/Material UI
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
+    <>
+      <Typography variant="subtitle2" align="center" gutterBottom>
+        {"Copyright © "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+      <Typography variant="subtitle2" align="center">
+        <Link color="inherit" href="https://mui.com/">
+          powered by React/Material UI
+        </Link>{" "}
+      </Typography>
+    </>
   );
 }
 
 export default function Footer() {
   return (
-    <>
       <Box sx={{ p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography variant="subtitle1" align="center" component="p">
-          Something here to give the footer a purpose!
-        </Typography>
         <Copyright />
       </Box>
-    </>
   );
 }
